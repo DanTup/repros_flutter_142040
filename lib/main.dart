326b627311e529d4968743f0c88b0d2879420bcf
Danny Tuppeny <danny@tuppeny.com>
2025-01-08 11:41:46 +0000

    Add repro

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      title: 'Flutter Demo',
+      home: Scaffold(body: SelectableText('This is selectable text')),
     );
   }
 }
